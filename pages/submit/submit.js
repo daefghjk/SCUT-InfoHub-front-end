68399e6e9e627d9d145c621d14c585de544269c2
Persuing-code <1400924709@qq.com>
2025-01-04 19:47:18 +0800

    19.50

--- pages/submit/submit.js
+++ pages/submit/submit.js
@@ -1,4 +1,3 @@
-// pages/submit/submit.js
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 const app = getApp()
 Page({
