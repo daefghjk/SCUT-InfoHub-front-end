d646c5ab8e24a8d6cde4f4c5954a68a06bc2cb7d
Persuing-code <1400924709@qq.com>
2025-01-04 22:09:43 +0800

    合并66

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,16 +1,8 @@
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 const app = getApp()
 Page({
-    /*data: {
-        text1: '',
-        name: '',
-        content: '',
-        log: 1,
-        name: app.globalData.userInfo.name,
-        open_id: '',
-        content: '',
-        time: '',
-        like: 0,
+    data: {
+        post:[],
         log: 1
     },
     async onLoad() {
@@ -20,38 +12,45 @@ Page({
         const res = await app.call({
             path: '/posts/',
         })
+        let post=[];
+        for(let i=0;i<=res.data.results.length-1;i++){
+            const a = await app.call({
+                path: '/users/',
+                method: 'GET',
+                data: {
+                    openid:res.data.results[i].poster
+                }
+            })
+            post[i].content=res.data.results[i].content,
+            post[i].name=a.data.name,
+            post[i].like=res.data.results[i].like,
+            post[i].time=res.data.results[i].time,
+            post[i].open_id=res.data.results[i].open_id
+        }
         this.setData({
-            content: res.data.results[0].content
+            post:post
         });
-        this.setData({
-            like: res.data.results[0].likes_count
-        });
-        this.setData({
-            time: res.data.results[0].create_time
-        });
-        this.setData({
-            open_id: res.data.results[0].open_id
-        });
-        this.data.text1 = app.globalData.userInfo.post_text
-        console.log(res.data)
-        console.log("123")
-        console.log(app.globalData.userInfo.post_text)
-
-    },
-    card() {
-        wx.navigateTo({
-            url: '/pages/cmentsec/cmentsec',
-        })
 
     },
     subscribe() {
-        app.call({
-            path: '/api/fans/follow/open_id/',
+        const a = app.call({
+            path: '/api/fans/follow/' + open_id + '/',
             method: 'POST'
         })
+        if (a.statusCode != 401) {
+            app.call({
+                path: '/api/fans/unfollow/' + open_id + '/',
+                method: 'POST'
+            })
+        }
         console.log("关注操作")
+
     },
+
+
+
     like() {
+
         app.call({
             path: '/posts/2/like/',
             method: 'POST'
@@ -62,5 +61,55 @@ Page({
         wx.navigateTo({
             url: '/pages/submit/submit',
         })
-    },*/
+
+    },
+    
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
 })
